5ae21e9ed9fc416f93822e08c2eec17e4ac2419b
Joshua Chase <jimmy58663@hotmail.com>
2026-05-21 14:39:07 -0700

    Add Phalanx reaction handling to gear equip and disable logic

--- common/Common_Funcs.lua
+++ common/Common_Funcs.lua
@@ -68,4 +68,28 @@ function Common_Funcs.Print_Table(tbl, prefix)
 	end
 end
 
+-- Reaction code for Phalanx
+windower.register_event("action", function(act)
+	local actor = windower.ffxi.get_mob_by_id(act.actor_id)
+	local self = windower.ffxi.get_player()
+	local category = act.category
+	-- Category 8 is Casting Start: https://github.com/Windower/Lua/blob/dev/addons/libs/packets/fields.lua#L1800-L1813
+	if category == 8 then
+		local spellID = act.targets[1].actions[1].param
+		local spell = res.spells[spellID]
+		if spell and (spell.en == "Phalanx" or spell.en == "Phalanx II") then
+			if Common_Funcs.Is_In_Party(act.targets[1].id) then
+				if sets.Midcast.Phalanx then
+					equip(sets.Midcast.Phalanx)
+					disable("head", "body", "hands", "legs", "feet")
+					coroutine.schedule(function()
+						enable("head", "body", "hands", "legs", "feet")
+						windower.send_command("gs c update")
+					end, 3)
+				end
+			end
+		end
+	end
+end)
+
 return Common_Funcs
