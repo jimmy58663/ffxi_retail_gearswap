fef7cca5845b7a23b68ce29cea2660e79a4141e4
Joshua Chase <jimmy58663@hotmail.com>
2026-07-01 23:01:22 -0700

    Refactor party_indices for compactness and update aftercast function call in Common_Funcs

--- common/Functions.lua
+++ common/Functions.lua
@@ -3,24 +3,7 @@ local texts = require("texts")
 local Common_Funcs = {}
 
 local party_indices = S {
-	"p0",
-	"p1",
-	"p2",
-	"p3",
-	"p4",
-	"p5",
-	"a10",
-	"a11",
-	"a12",
-	"a13",
-	"a14",
-	"a15",
-	"a20",
-	"a21",
-	"a22",
-	"a23",
-	"a24",
-	"a25",
+	"p0", "p1", "p2", "p3", "p4", "p5", "a10", "a11", "a12", "a13", "a14", "a15", "a20", "a21", "a22", "a23", "a24", "a25",
 }
 
 function Common_Funcs.Is_In_Party(id)
@@ -48,7 +31,8 @@ function Common_Funcs.Drop_Item(item_name)
 end
 
 function Common_Funcs.Update_Gear()
-	aftercast()
+	local spell = {}
+	aftercast(spell)
 end
 
 function Common_Funcs.Print_Table(tbl, prefix)
@@ -67,29 +51,5 @@ function Common_Funcs.Print_Table(tbl, prefix)
 		end
 	end
 end
-
--- Reaction code for Phalanx
-windower.register_event("action", function(act)
-	local actor = windower.ffxi.get_mob_by_id(act.actor_id)
-	local self = windower.ffxi.get_player()
-	local category = act.category
-	-- Category 8 is Casting Start: https://github.com/Windower/Lua/blob/dev/addons/libs/packets/fields.lua#L1800-L1813
-	if category == 8 then
-		local spellID = act.targets[1].actions[1].param
-		local spell = res.spells[spellID]
-		if spell and (spell.en == "Phalanx" or spell.en == "Phalanx II") then
-			if Common_Funcs.Is_In_Party(act.targets[1].id) then
-				if sets.Midcast.Phalanx then
-					equip(sets.Midcast.Phalanx)
-					disable("head", "body", "hands", "legs", "feet")
-					coroutine.schedule(function()
-						enable("head", "body", "hands", "legs", "feet")
-						windower.send_command("gs c update")
-					end, 3)
-				end
-			end
-		end
-	end
-end)
 
 return Common_Funcs
